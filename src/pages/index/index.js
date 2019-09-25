4fc3d8687d9cf66ec7a0c769d1c72ea3252bec62
Alexey Logachev <kingofrootelement@gmail.com>
2019-09-25 22:00:50 +0300

    Added text field and dropdown

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,9 @@
 import createMenu from "../../components/menu/menu";
 import './index.scss';
 
+import '../../components/text-field/text-field';
+import '../../components/drop-down/drop-down';
+
 const menu = createMenu(['Main', 'Index'], 'menu');
 document.body.appendChild(menu);
 
